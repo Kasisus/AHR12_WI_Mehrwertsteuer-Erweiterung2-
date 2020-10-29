dde17f37ef06c2ac005aaacc017d6009303eddea
Kasisus <73264146+Kasisus@users.noreply.github.com>
2020-10-29 15:19:25 +0100

    Fertig

---
+++ AHR12_WI_Mehrwertsteuer/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace AHR12_WI_Mehrwertsteuer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBruttobetrag19 = new System.Windows.Forms.TextBox();
+            this.txtMwStBetrag19 = new System.Windows.Forms.TextBox();
+            this.txtNettobetrag = new System.Windows.Forms.TextBox();
+            this.btnBerechnen = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtArtikel = new System.Windows.Forms.TextBox();
+            this.rtbAusgabe = new System.Windows.Forms.RichTextBox();
+            this.txtBruttobetrag16 = new System.Windows.Forms.TextBox();
+            this.txtMwStBetrag16 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mehrwertsteuer-Rechner";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 99);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Bruttobetrag in EUR:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 132);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "MwSt-Betrag in EUR:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 164);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nettobetrag in EUR:";
+            // 
+            // txtBruttobetrag19
+            // 
+            this.txtBruttobetrag19.Location = new System.Drawing.Point(133, 97);
+            this.txtBruttobetrag19.Margin = new System.Windows.Forms.Padding(2);
+            this.txtBruttobetrag19.Name = "txtBruttobetrag19";
+            this.txtBruttobetrag19.Size = new System.Drawing.Size(68, 20);
+            this.txtBruttobetrag19.TabIndex = 5;
+            // 
+            // txtMwStBetrag19
+            // 
+            this.txtMwStBetrag19.Location = new System.Drawing.Point(133, 130);
+            this.txtMwStBetrag19.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMwStBetrag19.Name = "txtMwStBetrag19";
+            this.txtMwStBetrag19.Size = new System.Drawing.Size(68, 20);
+            this.txtMwStBetrag19.TabIndex = 6;
+            // 
+            // txtNettobetrag
+            // 
+            this.txtNettobetrag.Location = new System.Drawing.Point(133, 162);
+            this.txtNettobetrag.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNettobetrag.Name = "txtNettobetrag";
+            this.txtNettobetrag.Size = new System.Drawing.Size(68, 20);
+            this.txtNettobetrag.TabIndex = 7;
+            // 
+            // btnBerechnen
+            // 
+            this.btnBerechnen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBerechnen.Location = new System.Drawing.Point(20, 244);
+            this.btnBerechnen.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBerechnen.Name = "btnBerechnen";
+            this.btnBerechnen.Size = new System.Drawing.Size(183, 32);
+            this.btnBerechnen.TabIndex = 8;
+            this.btnBerechnen.Text = "Berechnen";
+            this.btnBerechnen.UseVisualStyleBackColor = true;
+            this.btnBerechnen.Click += new System.EventHandler(this.btnBerechnen_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(133, 78);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "19 % MwSt";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Artikel:";
+            // 
+            // txtArtikel
+            // 
+            this.txtArtikel.Location = new System.Drawing.Point(132, 48);
+            this.txtArtikel.Margin = new System.Windows.Forms.Padding(2);
+            this.txtArtikel.Name = "txtArtikel";
+            this.txtArtikel.Size = new System.Drawing.Size(168, 20);
+            this.txtArtikel.TabIndex = 11;
+            // 
+            // rtbAusgabe
+            // 
+            this.rtbAusgabe.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbAusgabe.Location = new System.Drawing.Point(208, 203);
+            this.rtbAusgabe.Name = "rtbAusgabe";
+            this.rtbAusgabe.Size = new System.Drawing.Size(294, 73);
+            this.rtbAusgabe.TabIndex = 12;
+            this.rtbAusgabe.Text = "";
+            // 
+            // txtBruttobetrag16
+            // 
+            this.txtBruttobetrag16.Location = new System.Drawing.Point(232, 96);
+            this.txtBruttobetrag16.Margin = new System.Windows.Forms.Padding(2);
+            this.txtBruttobetrag16.Name = "txtBruttobetrag16";
+            this.txtBruttobetrag16.Size = new System.Drawing.Size(68, 20);
+            this.txtBruttobetrag16.TabIndex = 13;
+            // 
+            // txtMwStBetrag16
+            // 
+            this.txtMwStBetrag16.Location = new System.Drawing.Point(232, 130);
+            this.txtMwStBetrag16.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMwStBetrag16.Name = "txtMwStBetrag16";
+            this.txtMwStBetrag16.Size = new System.Drawing.Size(68, 20);
+            this.txtMwStBetrag16.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(235, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "16% MwSt";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(20, 203);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 32);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Alles Entfernen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(329, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(173, 174);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 281);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtMwStBetrag16);
+            this.Controls.Add(this.txtBruttobetrag16);
+            this.Controls.Add(this.rtbAusgabe);
+            this.Controls.Add(this.txtArtikel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnBerechnen);
+            this.Controls.Add(this.txtNettobetrag);
+            this.Controls.Add(this.txtMwStBetrag19);
+            this.Controls.Add(this.txtBruttobetrag19);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "MwSt-Rechner";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBruttobetrag19;
+        private System.Windows.Forms.TextBox txtMwStBetrag19;
+        private System.Windows.Forms.TextBox txtNettobetrag;
+        private System.Windows.Forms.Button btnBerechnen;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtArtikel;
+        private System.Windows.Forms.RichTextBox rtbAusgabe;
+        private System.Windows.Forms.TextBox txtBruttobetrag16;
+        private System.Windows.Forms.TextBox txtMwStBetrag16;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ AHR12_WI_Mehrwertsteuer/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AHR12_WI_Mehrwertsteuer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBerechnen_Click(object sender, EventArgs e)
+        {
+            double bruttobetrag19Eingabe = 0.0, mwstbetrag19Eingabe = 0.0, nettobetragEingabe = 0.0, bruttobetrag16Eingabe = 0.0, mwstbetrag16Eingabe = 0.0;
+            if (txtBruttobetrag19.Text != "") bruttobetrag19Eingabe = Convert.ToDouble(txtBruttobetrag19.Text);
+            double bruttobetrag19Ausgabe = 0.0;
+            if (txtMwStBetrag19.Text != "") mwstbetrag19Eingabe = Convert.ToDouble(txtMwStBetrag19.Text);
+            double mwstbetrag19Ausgabe = 0.0;
+            if (txtNettobetrag.Text != "") nettobetragEingabe = Convert.ToDouble(txtNettobetrag.Text);
+            double nettobetrag19Ausgabe = 0.0;
+            if (txtBruttobetrag16.Text != "") bruttobetrag16Eingabe = Convert.ToDouble(txtBruttobetrag16.Text);
+            double bruttobetrag16Ausgabe = 0.0;
+            if (txtMwStBetrag16.Text != "") mwstbetrag16Eingabe = Convert.ToDouble(txtMwStBetrag16.Text);
+            double mwstbetrag16Ausgabe = 0.0;
+            string artikel = txtArtikel.Text, ausgabe = "";
+         
+            if (bruttobetrag19Eingabe != 0)
+
+            {
+               
+                bruttobetrag19Ausgabe = bruttobetrag19Eingabe;
+                bruttobetrag16Ausgabe = bruttobetrag19Eingabe / 119 * 116;
+                mwstbetrag19Ausgabe = bruttobetrag19Eingabe / 119 * 19;
+                nettobetrag19Ausgabe = bruttobetrag19Eingabe / 119 * 100;
+                mwstbetrag16Ausgabe = bruttobetrag19Eingabe / 119 * 16;
+                ausgabe = "Der Netto Preis für den Artikel " + artikel + " beträgt " + Math.Round(nettobetrag19Ausgabe) + "EUR." + "\n" + "\n" + "Der neue Bruttobetrag mit 16% MwSt beträgt " + Math.Round(bruttobetrag16Ausgabe, 2) + "EUR.";
+
+
+                txtMwStBetrag19.Text = Convert.ToString(Math.Round(mwstbetrag19Ausgabe, 2));
+                txtNettobetrag.Text = Convert.ToString(Math.Round(nettobetrag19Ausgabe, 2));
+                txtMwStBetrag16.Text = Convert.ToString(Math.Round(mwstbetrag16Ausgabe, 2));
+                txtBruttobetrag16.Text = Convert.ToString(Math.Round(bruttobetrag16Ausgabe, 2));
+                txtBruttobetrag19.Text = Convert.ToString(Math.Round(bruttobetrag19Ausgabe, 2));
+
+                rtbAusgabe.Text = ausgabe;
+
+
+            }
+            else
+            {
+                if (bruttobetrag16Eingabe != 0)
+
+                {
+                    bruttobetrag16Ausgabe = bruttobetrag16Eingabe;
+                    bruttobetrag19Ausgabe = bruttobetrag16Eingabe / 116 * 119;
+                    mwstbetrag19Ausgabe = bruttobetrag16Eingabe / 116 * 19;
+                    nettobetrag19Ausgabe = bruttobetrag16Eingabe / 116 * 100;
+                    mwstbetrag16Ausgabe = bruttobetrag16Eingabe / 116 * 16;
+                    ausgabe = "Der Netto Preis für den Artikel " + artikel + " beträgt " + Math.Round(nettobetrag19Ausgabe) + "EUR." + "\n" + "\n" + "Der neue Bruttobetrag mit 16% MwSt beträgt " + Math.Round(bruttobetrag16Ausgabe, 2) + "EUR.";
+
+
+                    txtMwStBetrag19.Text = Convert.ToString(Math.Round(mwstbetrag19Ausgabe, 2));
+                    txtNettobetrag.Text = Convert.ToString(Math.Round(nettobetrag19Ausgabe, 2));
+                    txtMwStBetrag16.Text = Convert.ToString(Math.Round(mwstbetrag16Ausgabe, 2));
+                    txtBruttobetrag16.Text = Convert.ToString(Math.Round(bruttobetrag16Ausgabe, 2));
+                    txtBruttobetrag19.Text = Convert.ToString(Math.Round(bruttobetrag19Ausgabe, 2));
+
+                    rtbAusgabe.Text = ausgabe;
+
+                 
+                }
+                else
+                {
+                    if (mwstbetrag19Eingabe != 0)
+
+                    {
+                        mwstbetrag19Ausgabe = mwstbetrag19Eingabe;
+                        bruttobetrag19Ausgabe = mwstbetrag19Eingabe / 19 * 119;
+                        bruttobetrag16Ausgabe = mwstbetrag19Eingabe / 19 * 116;
+                        nettobetrag19Ausgabe = mwstbetrag19Eingabe / 19 * 100;
+                        mwstbetrag16Ausgabe = mwstbetrag19Eingabe / 19 * 16;
+                        ausgabe = "Der Netto Preis für den Artikel " + artikel + " beträgt " + Math.Round(nettobetrag19Ausgabe) + "EUR." + "\n" + "\n" + "Der neue Bruttobetrag mit 16% MwSt beträgt " + Math.Round(bruttobetrag16Ausgabe, 2) + "EUR.";
+
+
+                        txtMwStBetrag19.Text = Convert.ToString(Math.Round(mwstbetrag19Ausgabe, 2));
+                        txtNettobetrag.Text = Convert.ToString(Math.Round(nettobetrag19Ausgabe, 2));
+                        txtMwStBetrag16.Text = Convert.ToString(Math.Round(mwstbetrag16Ausgabe, 2));
+                        txtBruttobetrag16.Text = Convert.ToString(Math.Round(bruttobetrag16Ausgabe, 2));
+                        txtBruttobetrag19.Text = Convert.ToString(Math.Round(bruttobetrag19Ausgabe, 2));
+
+                        rtbAusgabe.Text = ausgabe;
+
+
+                    }
+                    else
+                    {
+                        if (mwstbetrag16Eingabe != 0)
+
+                        {
+                            mwstbetrag16Ausgabe = mwstbetrag16Eingabe;
+                            bruttobetrag19Ausgabe = mwstbetrag16Eingabe / 16 * 119;
+                            bruttobetrag16Ausgabe = mwstbetrag16Eingabe / 16 * 116;
+                            nettobetrag19Ausgabe = mwstbetrag16Eingabe / 16 * 100;
+                            mwstbetrag19Ausgabe = mwstbetrag16Eingabe / 16 * 19;
+                            ausgabe = "Der Netto Preis für den Artikel " + artikel + " beträgt " + Math.Round(nettobetrag19Ausgabe) + "EUR." + "\n" + "\n" + "Der neue Bruttobetrag mit 16% MwSt beträgt " + Math.Round(bruttobetrag16Ausgabe, 2) + "EUR.";
+
+
+                            txtMwStBetrag19.Text = Convert.ToString(Math.Round(mwstbetrag19Ausgabe, 2));
+                            txtNettobetrag.Text = Convert.ToString(Math.Round(nettobetrag19Ausgabe, 2));
+                            txtMwStBetrag16.Text = Convert.ToString(Math.Round(mwstbetrag16Ausgabe, 2));
+                            txtBruttobetrag16.Text = Convert.ToString(Math.Round(bruttobetrag16Ausgabe, 2));
+                            txtBruttobetrag19.Text = Convert.ToString(Math.Round(bruttobetrag19Ausgabe, 2));
+
+                            rtbAusgabe.Text = ausgabe;
+
+                            
+
+                        }
+                        else
+                        {
+                            if (nettobetragEingabe != 0)
+
+                            {
+                                nettobetrag19Ausgabe = nettobetragEingabe;
+                                bruttobetrag19Ausgabe = nettobetragEingabe * 1.19;
+                                bruttobetrag16Ausgabe = nettobetragEingabe * 1.16;
+                                mwstbetrag19Ausgabe = nettobetragEingabe * 0.19;
+                                mwstbetrag16Ausgabe = nettobetragEingabe * 0.16;
+                                ausgabe = "Der Netto Preis für den Artikel " + artikel + " beträgt " + Math.Round(nettobetrag19Ausgabe, 2) + "EUR." + "\n" + "\n" + "Der neue Bruttobetrag mit 16% MwSt beträgt " + Math.Round(bruttobetrag16Ausgabe, 2) + "EUR.";
+
+
+                                txtMwStBetrag19.Text = Convert.ToString(Math.Round(mwstbetrag19Ausgabe, 2));
+                                txtNettobetrag.Text = Convert.ToString(Math.Round(nettobetrag19Ausgabe, 2));
+                                txtMwStBetrag16.Text = Convert.ToString(Math.Round(mwstbetrag16Ausgabe, 2));
+                                txtBruttobetrag16.Text = Convert.ToString(Math.Round(bruttobetrag16Ausgabe, 2));
+                                txtBruttobetrag19.Text = Convert.ToString(Math.Round(bruttobetrag19Ausgabe, 2));
+
+                                rtbAusgabe.Text = ausgabe;
+
+                              
+
+                            }
+                        }
+                    }
+                }
+            }
+            rtbAusgabe.Select(32, artikel.Length);
+            rtbAusgabe.SelectionColor = Color.LightGoldenrodYellow;
+            rtbAusgabe.SelectionBackColor = Color.Black;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            txtMwStBetrag19.Text = "";
+            txtNettobetrag.Text = "";
+            txtMwStBetrag16.Text = "";
+            txtBruttobetrag16.Text = "";
+            txtBruttobetrag19.Text = "";
+            txtArtikel.Text = "";
+            rtbAusgabe.Text = "";
+        }
+    }
+}
